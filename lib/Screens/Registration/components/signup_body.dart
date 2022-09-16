919a7c710013df4fb8f4a981fd75c3985b3e1d80
Cesar Edgardo Bicol Jr <cesar.bicol@gmail.com>
2022-09-17 04:29:33 +0800

    Sign Up Page Added

--- lib/Screens/Registration/components/signup_body.dart
+++ lib/Screens/Registration/components/signup_body.dart
@@ -1,7 +1,15 @@
 import 'package:ehatid_passenger_app/Screens/Registration/components/signup_bg.dart';
+import 'package:ehatid_passenger_app/Screens/Registration/sign_up.dart';
 import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:ehatid_passenger_app/FadeAnimation.dart';
 
-class SignUpBody extends StatelessWidget{
+class SignUpBody extends StatefulWidget {
+  @override
+  _SignUpBodyState createState() => _SignUpBodyState();
+}
+
+class _SignUpBodyState extends State<SignUpBody> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -40,6 +48,94 @@ class SignUpBody extends StatelessWidget{
               fontWeight: FontWeight.w500,
               color: Color.fromARGB(255, 39, 39, 39),
               letterSpacing: -0.48,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(30, 10, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget> [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color(0xffeeeeee),
+                            blurRadius: 10,
+                            offset: Offset(0,4)
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(color: Colors.black.withOpacity(0.13))
+                  ),
+                  child: Stack(
+                    children: [
+                      InternationalPhoneNumberInput(
+                        onInputChanged: (value) {},
+                        cursorColor: Colors.black,
+                        formatInput: false,
+                        initialValue: PhoneNumber(isoCode: 'PH', dialCode:'+63'),
+                        selectorConfig: SelectorConfig(
+                            selectorType: PhoneInputSelectorType.BOTTOM_SHEET
+                        ),
+                        inputDecoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(bottom: 15, left: 0),
+                            border: InputBorder.none,
+                            hintText: 'Phone Number',
+                            hintStyle: TextStyle(
+                                color: Colors.grey.shade500, fontFamily: 'Montserrat', fontSize: 16, fontWeight: FontWeight.w400
+                            )
+                        ),
+                      ),
+                      Positioned(
+                        left: 85,
+                        top: 8,
+                        bottom:8,
+                        child: Container(
+                          height: 40,
+                          width: 1,
+                          color: Colors.black.withOpacity(0.13),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp()));
+                  },
+                  color: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                  minWidth: double.infinity,
+                  child: Text("Sign up with Phone", style: TextStyle(
+                      color: Colors.white, fontFamily: 'Montserrat', fontSize: 14
+                  ),),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 90, top: 15),
+                  child: GestureDetector(
+                    child: Text("I have an account already",
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        height: 1.171875,
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromARGB(255, 39, 39, 39),
+                        decoration: TextDecoration.underline,
+                        letterSpacing: -0.48,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)=>SignUp()));
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/Screens/Registration/sign_up.dart
+++ lib/Screens/Registration/sign_up.dart
@@ -1,14 +1,7 @@
 import 'package:ehatid_passenger_app/Screens/Registration/components/signup_body.dart';
 import 'package:flutter/material.dart';
 
-class SignUp extends StatefulWidget {
-  const SignUp({Key? key}) : super(key: key);
-
-  @override
-  _SignUp createState() => _SignUp();
-}
-class _SignUp extends State<SignUp> {
-
+class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: SignUpBody(),);
