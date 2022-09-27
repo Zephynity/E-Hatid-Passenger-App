754ebacde715789e30422eec2e965296ac296644
Cesar Edgardo Bicol Jr <cesar.bicol@gmail.com>
2022-09-27 15:46:01 +0800

    Try Map

--- lib/test_map.dart
+++ lib/test_map.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:ehatid_passenger_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TestMap extends StatefulWidget {
@@ -16,6 +18,33 @@ class _TestMapState extends State<TestMap> {
   static const LatLng sourceLocation = LatLng(37.33500962, -122.03272188);
   static const LatLng destination = LatLng(37.33429383, -122.06600055);
 
+  List<LatLng> polylineCoordinates = [];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        google_api_key,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(destination.latitude, destination.longitude)
+    );
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude)),
+      );
+      setState(() {
+
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,8 +54,28 @@ class _TestMapState extends State<TestMap> {
         ),
       ),
       body: GoogleMap(
-        initialCameraPosition:
-          CameraPosition(target: sourceLocation, zoom: 14.5),
+        initialCameraPosition: CameraPosition(
+          target: sourceLocation,
+          zoom: 14.5,
+        ),
+        polylines: {
+          Polyline(
+            polylineId: PolylineId("route"),
+            points: polylineCoordinates,
+            color: Color(0xFF7B61FF),
+            width: 6,
+          ),
+        },
+        markers: {
+          const Marker(
+            markerId: MarkerId("source"),
+            position: sourceLocation,
+          ),
+          const Marker(
+            markerId: MarkerId("destination"),
+            position: destination,
+          ),
+        },
       ),
     );
   }
